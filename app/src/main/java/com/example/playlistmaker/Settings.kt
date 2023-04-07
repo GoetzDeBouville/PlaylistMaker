3cd4252536f06e8d2eb9e05d2dcd217bbf0ddd31
GoetzLeHavre <count.of.tuscan@gmail.com>
2023-04-07 12:44:13 +0300

    fixed returning problem

--- app/src/main/java/com/example/playlistmaker/Settings.kt
+++ app/src/main/java/com/example/playlistmaker/Settings.kt
@@ -1,9 +1,7 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
 
 class Settings : AppCompatActivity() {
@@ -14,8 +12,7 @@ class Settings : AppCompatActivity() {
         val arrowBackButton = findViewById<ImageView>(R.id.arrow_back)
 
         arrowBackButton.setOnClickListener {
-            val intent = Intent(this@Settings, MainActivity::class.java)
-            startActivity(intent)
+            finish()
         }
     }
 }
