8bd67f1353d84fb1f6bfac52b2a02ed19c78c163
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-05-03 19:06:20 +0300

    Исключен неиспользуемый импорт

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -7,7 +7,6 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 
