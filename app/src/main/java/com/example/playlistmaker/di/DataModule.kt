6d36be39487bfb6ea2d27e8bec73832377f685d0
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-08-24 01:48:47 +0300

    корректировка dataModule и RetrofitNetworkClient

--- app/src/main/java/com/example/playlistmaker/di/DataModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/DataModule.kt
@@ -1,6 +1,8 @@
 package com.example.playlistmaker.di
 
+import android.content.Context
 import com.example.playlistmaker.data.player.PlayerImpl
+import com.example.playlistmaker.data.search.network.ItunesAPI
 import com.example.playlistmaker.data.search.network.NetworkClient
 import com.example.playlistmaker.data.search.network.RetrofitNetworkClient
 import com.example.playlistmaker.data.search.storage.History
@@ -11,11 +13,30 @@ import com.example.playlistmaker.domain.player.Player
 import com.example.playlistmaker.domain.search.models.Track
 import com.example.playlistmaker.domain.sharing.ContentProvider
 import com.example.playlistmaker.domain.sharing.ExternalNavigator
+import com.google.gson.Gson
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val dataModule = module {
-    single<NetworkClient> { RetrofitNetworkClient(androidContext()) }
+
+    single<ItunesAPI> {
+        Retrofit.Builder()
+            .baseUrl(RetrofitNetworkClient.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ItunesAPI::class.java)
+    }
+
+    single {
+        androidContext()
+            .getSharedPreferences("pm_prefs", Context.MODE_PRIVATE)
+    }
+
+    factory { Gson() }
+
+    single<NetworkClient> { RetrofitNetworkClient(get(), androidContext()) }
 
     factory<History> { SharedPreferencesHistoryStorage(get()) }
 
