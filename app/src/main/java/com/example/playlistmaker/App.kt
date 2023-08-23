97a22bbdbb168e38fa2c93046c56aa3525601e46
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-08-24 01:50:55 +0300

    корректировка dataModule и RetrofitNetworkClient

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -1,7 +1,6 @@
 package com.example.playlistmaker
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import com.example.playlistmaker.di.dataModule
 import com.example.playlistmaker.di.interactorModule
 import com.example.playlistmaker.di.repositoryModule
@@ -9,12 +8,8 @@ import com.example.playlistmaker.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import java.util.logging.Level
 
 class App : Application() {
-
-//    var darkTheme = false
-
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -23,16 +18,5 @@ class App : Application() {
             modules(listOf(dataModule, repositoryModule, interactorModule, viewModelModule))
         }
     }
-
-//    fun switchTheme(darkThemeEnabled: Boolean) {
-//        darkTheme = darkThemeEnabled
-//        AppCompatDelegate.setDefaultNightMode(
-//            if (darkThemeEnabled) {
-//                AppCompatDelegate.MODE_NIGHT_YES
-//            } else {
-//                AppCompatDelegate.MODE_NIGHT_NO
-//            }
-//        )
-//    }
 }
 
