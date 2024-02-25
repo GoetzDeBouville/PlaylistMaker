418f478a2ad7b5fac7bc06e339a74b289b890a3d
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-02-25 15:54:46 +0300

    Добавляет инициализацию  view и переопределяет метод onDraw()

--- app/src/main/java/com/example/playlistmaker/utils/views/PlaybackControlView.kt
+++ app/src/main/java/com/example/playlistmaker/utils/views/PlaybackControlView.kt
@@ -1,10 +1,14 @@
 package com.example.playlistmaker.utils.views
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
+import androidx.core.graphics.drawable.toBitmap
 import com.example.playlistmaker.R
 import kotlin.math.min
 
@@ -15,6 +19,25 @@ class PlaybackControlView @JvmOverloads constructor(
     @StyleRes defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleAttr) {
     private val minViewSize = resources.getDimensionPixelSize(R.dimen.circularProgressViewMinSize)
+
+    private val imageBitmap: Bitmap?
+    private var imageRect = RectF(0f, 0f, 0f, 0f)
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs, R.styleable.PlaybackControlView, defStyleAttr, defStyleRes
+        ).apply {
+            try {
+                val playImg = getDrawable(R.styleable.PlaybackControlView_playButtonId)
+                val pauseImg = getDrawable(R.styleable.PlaybackControlView_pauseButtonId)
+                val isPlaying = getBoolean(R.styleable.PlaybackControlView_isPlaying, false)
+                imageBitmap = if (isPlaying) pauseImg?.toBitmap() else playImg?.toBitmap()
+            } finally {
+                recycle()
+            }
+        }
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val widthSize = MeasureSpec.getSize(widthMeasureSpec)
         val contentWith = when (MeasureSpec.getMode(widthMeasureSpec)) {
@@ -34,5 +57,12 @@ class PlaybackControlView @JvmOverloads constructor(
 
         val size = min(contentWith, contentHeight)
         setMeasuredDimension(size, size)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (imageBitmap != null) {
+            val rect = RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
+            canvas.drawBitmap(imageBitmap, null, rect, null)
+        }
     }
 }
