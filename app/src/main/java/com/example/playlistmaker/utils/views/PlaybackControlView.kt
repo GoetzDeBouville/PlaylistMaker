9f15155b523abc5b997e6ec340dc711319539152
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-02-25 15:47:16 +0300

    Добавляет класс кастомной view и устанавливает его на макет верстки

---
+++ app/src/main/java/com/example/playlistmaker/utils/views/PlaybackControlView.kt
@@ -0,0 +1,22 @@
+package com.example.playlistmaker.utils.views
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import androidx.core.graphics.drawable.toBitmap
+import com.example.playlistmaker.R
+import kotlin.math.min
+
+class PlaybackControlView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0,
+    @StyleRes defStyleRes: Int = 0
+) : View(context, attrs, defStyleAttr, defStyleAttr) {
+
+}
