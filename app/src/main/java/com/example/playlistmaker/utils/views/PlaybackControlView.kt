45a4a445eac051d54d7e5569c5fe46ded409bd4b
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-02-25 15:53:06 +0300

    Переопределяет метод onMeasure для кастомной view

--- app/src/main/java/com/example/playlistmaker/utils/views/PlaybackControlView.kt
+++ app/src/main/java/com/example/playlistmaker/utils/views/PlaybackControlView.kt
@@ -1,14 +1,10 @@
 package com.example.playlistmaker.utils.views
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
-import androidx.core.graphics.drawable.toBitmap
 import com.example.playlistmaker.R
 import kotlin.math.min
 
@@ -18,5 +14,25 @@ class PlaybackControlView @JvmOverloads constructor(
     @AttrRes defStyleAttr: Int = 0,
     @StyleRes defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleAttr) {
+    private val minViewSize = resources.getDimensionPixelSize(R.dimen.circularProgressViewMinSize)
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val contentWith = when (MeasureSpec.getMode(widthMeasureSpec)) {
+            MeasureSpec.UNSPECIFIED -> minViewSize
+            MeasureSpec.AT_MOST -> widthSize
+            MeasureSpec.EXACTLY -> widthSize
+            else -> error("Unexpected size widthMode")
+        }
 
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        val contentHeight = when (MeasureSpec.getMode(heightMeasureSpec)) {
+            MeasureSpec.UNSPECIFIED -> minViewSize
+            MeasureSpec.AT_MOST -> heightSize
+            MeasureSpec.EXACTLY -> heightSize
+            else -> error("Unexpected size heightMode")
+        }
+
+        val size = min(contentWith, contentHeight)
+        setMeasuredDimension(size, size)
+    }
 }
