b783c7b91e49776749ead7355cca59f1f8889b99
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-13 00:03:28 +0300

    подключает coil compose настраивает лоадер для coil

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -1,6 +1,17 @@
 package com.example.playlistmaker
 
 import android.app.Application
+import coil.ComponentRegistry
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.request.DefaultRequestOptions
+import coil.request.Disposable
+import coil.request.ImageRequest
+import coil.request.ImageResult
+import coil.util.DebugLogger
 import com.example.playlistmaker.di.dataModule
 import com.example.playlistmaker.di.interactorModule
 import com.example.playlistmaker.di.repositoryModule
@@ -11,7 +22,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class App : Application() {
+class App : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -23,5 +34,25 @@ class App : Application() {
         val settingsRepository: SettingsRepository by inject()
         val darkTheme = settingsRepository.getThemeSettings()
         settingsRepository.updateThemeSettings(darkTheme)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this).newBuilder()
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.05)
+                    .strongReferencesEnabled(true)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.03)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
     }
 }
